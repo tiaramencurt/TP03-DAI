6b78be7bec890d2598ed03adaee0b5a69341d5f6
Android Studio <devandroid@ort>
2026-04-10 12:08:33 -0300

    m

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-
 import Alumno from "./models/alumno.js";
 import {sumar, restar, multiplicar, dividir} from "./modules/matematica.js";
 import {OMDBSearchByPage, OMDBSearchComplete, OMDBGetByImdbID} from "./modules/omdb-wrapper.js";
@@ -21,6 +20,7 @@ app.get('/saludar/:nombre',(req,res)=>{
 
 app.get('/validarfecha/:ano/:mes/:dia',(req,res)=>{
     let fecha = req.params.ano + "-" + req.params.mes + "-" + req.params.dia;
+    console.log(fecha)
     let resultado = Date.parse(fecha);
     if(isNaN(resultado)){
         res.status(400).send("Fecha inválida");
@@ -38,65 +38,7 @@ app.get('/matematica/sumar',(req,res)=>{
     });
 });
 
-app.get('/matematica/dividir',(req,res)=>{
-    let n1 = Number(req.query.n1);
-    let n2 = Number(req.query.n2);
-    if(n2 == 0){
-        res.status(400).send("El divisor no puede ser cero");
-        return;
-    }
-    res.status(200).send({
-        resultado: dividir(n1,n2)
-    });
-});
 
-app.get('/omdb/searchcomplete', async (req,res)=>{
-    let search = req.query.search;
-    let resultado = await OMDBSearchComplete(search);
-    res.status(200).send(resultado);
-});
-
-const alumnosArray = [];
-
-alumnosArray.push(new Alumno("Esteban Dido","22888444",20));
-alumnosArray.push(new Alumno("Matias Queroso","28946255",51));
-alumnosArray.push(new Alumno("Elba Calao","32623391",18));
-
-app.get('/alumnos',(req,res)=>{
-    res.status(200).send(alumnosArray);
-});
-
-app.get('/alumnos/:dni',(req,res)=>{
-    let alumno = alumnosArray.find(a=>a.dni==req.params.dni);
-    if(alumno){
-        res.status(200).send(alumno);
-    }
-    else{
-        res.status(404).send("Alumno no encontrado");
-    }
-});
-
-app.post('/alumnos',(req,res)=>{
-    let nuevoAlumno = new Alumno(
-        req.body.username,
-        req.body.dni,
-        req.body.edad
-    );
-    alumnosArray.push(nuevoAlumno);
-    res.status(201).send("Alumno creado");
-});
-
-app.delete('/alumnos',(req,res)=>{
-    let index = alumnosArray.findIndex(a=>a.dni==req.body.dni);
-    if(index>=0){
-        alumnosArray.splice(index,1);
-        res.status(200).send("Alumno eliminado");
-    }
-    else{
-        res.status(404).send("Alumno no encontrado");
-    }
-
-});
 
 app.listen(port,()=>{
     console.log("Server corriendo");
